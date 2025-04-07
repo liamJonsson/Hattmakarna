dd66aac51b413ff21dd1e6cea8b2c74ca85e4345
liamJonsson <limme98@gmail.com>
2025-04-07 21:45:50 +0200

    Hattmakarna
    Databasuppkoppling

---
+++ Hattmakarna/src/hattmakarna/Hattmakarna.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hattmakarna;
+import oru.inf.InfDB;
+import oru.inf.InfException;
+/**
+ *
+ * @author limme
+ */
+public class Hattmakarna {
+
+    private static InfDB idb; //skapar ett fält av klassen InfDB. Fältet som man vill lagra databasuppkopplingen i
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try{
+            idb = new InfDB("hattmakarna","3306","dbAdmin2024","dbAdmin2024PW"); //Namnet på databasen, vilken port,användarnamnet till databasen, lösenordet för användaren
+            //new Inloggning(idb).setVisible(true);
+        }
+        catch(InfException ex){ //om någotning går fel i uppkopplingen av databasen så kommer variabeln ex fånga upp de specifika felmeddelandena som finns i infdb-klassen
+            System.out.println(ex.getMessage());   
+        }
+    }
+    
+}
